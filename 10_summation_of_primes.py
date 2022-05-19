a4f4f581cd1a031e0d8be0ae6ef01cc902de85d8
Alex Pickering <alex.pickering15@gmail.com>
2022-05-19 15:17:51 -0400

    init added, 10 developed

--- 10_summation_of_primes.py
+++ 10_summation_of_primes.py
@@ -2,8 +2,18 @@ from 3_largest_prime_factor import prime_generator
 
 
 
+def prime_sum_to(limit)
+    gen = prime_generator()
+    prime_sum = 0
+    prime = 0
+    while prime < limit:
+        prime_sum += prime
+        prime = next(gen)
+
+
 def main():
-    pass
+    prime_sum_to(10)
+    #prime_sum_to(2000000)
 
 
 if __name__ == '__main__':
